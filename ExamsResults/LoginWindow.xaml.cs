9a14cc59c58a6c7571e8a0fc998758b380b3425d
Андрюха Смер АС. Смерч <Andro@CCJGMK.local>
2026-04-14 13:00:32 +0300

    first commit

---
+++ ExamsResults/LoginWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExamsResults
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        MainWindow _mainWindow = new MainWindow();
+        private void LoginBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if(Pass.Text != null && Email.Text != null)
+            {
+                _mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Введите почту или пароль", "Save error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
